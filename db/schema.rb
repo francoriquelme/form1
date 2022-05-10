cdab7663ae3385281a032ff163b504f23fa04d5e
francoriquelme <francoestebanru@gmail.com>
2022-05-09 21:33:42 -0400

    campos adicionales

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_232310) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_011619) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
   end
 
 end
